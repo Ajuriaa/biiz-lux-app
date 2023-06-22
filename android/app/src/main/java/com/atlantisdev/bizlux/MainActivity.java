ee84cc26f3b8ba1f96f0042aaf3ddc6d3635e30a
Jose Ajuria <alejoajuria@gmail.com>
2023-06-22 00:24:15 -0600

    capacitor added

---
+++ android/app/src/main/java/com/atlantisdev/bizlux/MainActivity.java
@@ -0,0 +1,5 @@
+package com.atlantisdev.bizlux;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.atlantisdev.bizlux',
+  appName: 'BIZ Lux',
+  webDir: 'dist/biz-lux-app',
+  bundledWebRuntime: false
+};
+
+export default config;
