b5b8fb929689994012948e941bb84155ea2c92b8
Shiro <guzh77@hotmail.com>
2023-06-28 00:38:19 -0600

    Profile-view

--- src/app/shared/profile/profile.module.ts
+++ src/app/shared/profile/profile.module.ts
@@ -2,13 +2,17 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProfileComponent } from './components';
+import { HeaderModule } from '../header/header.module';
+import { ButtonsModule } from '../buttons/buttons.module';
 
 @NgModule({
   declarations: [ProfileComponent],
   exports: [ProfileComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    HeaderModule,
+    ButtonsModule
   ]
 })
 export class ProfileModule { }
