3c4a9b7b749d415a084681a1d334a9b2fe2bd88f
Shiro <guzh77@hotmail.com>
2023-07-02 20:38:43 -0600

    tests fixed

--- src/app/shared/Faq/components/faq.component.spec.ts
+++ src/app/shared/Faq/components/faq.component.spec.ts
@@ -1,8 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute , RouterModule } from '@angular/router';
 import { HeaderModule } from 'src/app/shared';
-import { RouterModule } from '@angular/router';
-import { FaqComponent } from './faq.component';
+import { FaqComponent } from '../../faq/components/faq.component';
 
 describe('FaqComponent', () => {
   let component: FaqComponent;
