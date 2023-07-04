5f973601611843282c067b8605c07fc2fd94dec9
mel <jm.garcia1501@gmail.com>
2023-07-03 20:09:56 -0600

    create display global component

---
+++ src/app/shared/buttons/components/display-button/display-button.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-display-button',
+  templateUrl: './display-button.component.html',
+  styleUrls: ['./display-button.component.scss']
+})
+export class DisplayButtonComponent {
+  @Input() label = '';
+  @Input() routerLink = '';
+}
