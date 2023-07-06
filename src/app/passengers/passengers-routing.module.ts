ac70956f712e07bf715320e0486d335509de7da0
Shiro <guzh77@hotmail.com>
2023-07-06 15:10:14 -0600

    comments addresed

--- src/app/passengers/passengers-routing.module.ts
+++ src/app/passengers/passengers-routing.module.ts
@@ -35,7 +35,7 @@ const routes: Routes = [{
     },
     {
       path: 'forgot-something',
-      title: 'Olvide Algo',
+      title: 'Olvidé Algo',
       component: ForgotSomethingComponent
     }
   ]
