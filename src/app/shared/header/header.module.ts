6baff63e14c1bce4ce40e9a3f86bf3818cc6b937
mel <jm.garcia1501@gmail.com>
2023-07-06 18:08:13 -0600

    moved header to its own folder

--- src/app/shared/header/header.module.ts
+++ src/app/shared/header/header.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 
-import { HeaderComponent } from './components/header.component';
+import { HeaderComponent } from './components';
 
 @NgModule({
   declarations: [HeaderComponent],
