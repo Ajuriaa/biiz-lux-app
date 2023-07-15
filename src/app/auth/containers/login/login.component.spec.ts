fab41727635ee229bd7adfdc4846e8d944b67b1f
Shiro <guzh77@hotmail.com>
2023-07-15 17:55:11 -0600

    login view added

--- src/app/auth/containers/login/login.component.spec.ts
+++ src/app/auth/containers/login/login.component.spec.ts
@@ -1,15 +1,22 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [LoginComponent],
+      imports: [
+        ReactiveFormsModule
+      ],
+    })
+    .compileComponents();
+  });
+
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [LoginComponent]
-    });
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
--- src/app/auth/containers/login/login.component.ts
+++ src/app/auth/containers/login/login.component.ts
@@ -1,17 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public homePathname = '';
+  public emailForm: FormGroup = new FormGroup({});
 
   constructor(
-    private _router: Router
+    private _router: Router,
+    private readonly _formBuilder: FormBuilder
   ) {}
+
+  ngOnInit(): void {
+    this.emailForm = this._formBuilder.group({
+      email: ['', [Validators.required]],
+      pass: ['', [Validators.required]]
+    });
+  }
 
   public onSubmit(): void {
     // TODO: CHANGE THIS HARCODED PATH WHEN USERS ROLES ARE ADDED
@@ -19,3 +30,4 @@ export class LoginComponent {
     this._router.navigate([this.homePathname]);
   }
 }
+
