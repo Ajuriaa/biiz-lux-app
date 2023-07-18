6b8b80327d6a9de5fa732a67bf0431bb2b1b7bd6
Jose Ajuria <alejoajuria@gmail.com>
2023-07-17 19:58:36 -0600

    whiteline added

--- src/app/shared/buttons/buttons.module.ts
+++ src/app/shared/buttons/buttons.module.ts
@@ -1,14 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { 
+import {
   PrimaryButtonComponent,
   SecondaryButtonComponent,
   ToggleButtonComponent,
   DisplayButtonComponent,
   WhiteButtonComponent
 } from './components';
-
 
 @NgModule({
   declarations: [
