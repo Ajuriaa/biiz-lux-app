7f0096f95365184e3e1440eba24ee7043a5693a6
Jose Ajuria <alejoajuria@gmail.com>
2023-07-20 07:53:35 -0600

    header component name added and overall improvements to code

--- src/app/shared/header/components/profile-header/profile-header.component.ts
+++ src/app/shared/header/components/profile-header/profile-header.component.ts
@@ -1,12 +1,14 @@
 import { Component, Input } from '@angular/core';
 
+const DEFAULT_IMAGE_URL = 'https://biz-app-bucket.s3.us-east-2.amazonaws.com/profile-image.png';
+
 @Component({
   selector: 'app-profile-header',
   templateUrl: './profile-header.component.html',
   styleUrls: ['./profile-header.component.scss']
 })
 export class ProfileHeaderComponent {
-  @Input() imgSrc = '';
+  @Input() imgSrc = DEFAULT_IMAGE_URL;
   @Input() profileName = 'PROFILE NAME';
   @Input() showEditIcon = false;
 }
