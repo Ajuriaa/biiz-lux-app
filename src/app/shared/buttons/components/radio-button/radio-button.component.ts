e2a2f5fa998a0c7716ce76070274629fe2a24488
Shiro <guzh77@hotmail.com>
2023-08-11 15:57:36 -0600

    Comments addresed

--- src/app/shared/buttons/components/radio-button/radio-button.component.ts
+++ src/app/shared/buttons/components/radio-button/radio-button.component.ts
@@ -2,16 +2,11 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-radio-button',
-  template: `
-    <label class="radio-label">
-      <input type="radio" [value]="value" [name]="name" [checked]="value" class="green-radio">
-      {{ label }}
-    </label>
-  `,
+  templateUrl: './radio-button.component.html',
   styleUrls: ['./radio-button.component.scss']
 })
 export class RadioButtonComponent {
   @Input() label = '';
-  @Input() value: any = '';
+  @Input() value = '';
   @Input() name = '';
 }
