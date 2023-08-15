5740a717a7937a16135b10492723e146f715c7fc
Roger! <icarus.gk@gmail.com>
2023-08-15 11:51:53 -0600

    feat: add current user to `profile-header` component

--- src/app/shared/profile/components/profile/profile.component.ts
+++ src/app/shared/profile/components/profile/profile.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserInfoQueries } from "src/app/passengers/services";
+import { USER } from 'src/app/core/constants';
+import { IUser } from 'src/app/passengers/interfaces';
 
 @Component({
   selector: 'app-profile.component',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  public user: IUser = USER;
 
+  constructor(
+    private _userInfoQuery: UserInfoQueries
+  ) {}
+
+  ngOnInit() {
+    this._userInfoQuery.getUserInformation().subscribe(({ data }): void => {
+      if (data) {
+        this.user = data.currentUser;
+      }
+    })
+  }
 }
