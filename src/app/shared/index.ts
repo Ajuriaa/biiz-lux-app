7d071774b41ca2e78623ad67dc178523c759aece
Alejandro Ajuria <alejoajuria@gmail.com>
2023-08-30 12:10:03 -0600

    Biz 108 hacer responsive el componente de google maps (#59)
    * @angular/google-maps removed
    
    * capacitor maps added and synced
    
    * maps reposiveness fixed and view finished
    
    * whitespace removed

--- src/app/shared/index.ts
+++ src/app/shared/index.ts
@@ -4,5 +4,4 @@ export * from './profile/profile.module';
 export * from './customer-service/customer-service.module';
 export * from './faq/faq.module';
 export * from './calendar/calendar.module';
-export * from './maps/maps.module';
 export * from './toaster/toaster.module';
