d522f15925d9f7a682338ec53981ec026d7b4008
Alejandro Ajuria <alejoajuria@gmail.com>
2023-09-07 16:44:26 -0600

    maps in other component background fixed (#68)
    * maps in other component background fixed
    
    * profile header included in ion tag

--- src/app/shared/header/components/header/header.component.ts
+++ src/app/shared/header/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { WeatherQueries } from 'src/app/shared/services';
 
 const DEFAULT_WEATHER_IMAGE = 'assets/images/weather.svg';
+const TRANSPARENT_HEADER_ROUTES = ['trip', 'customer-service'];
 @Component({
   selector: 'app-shared-header',
   templateUrl: './header.component.html',
@@ -11,15 +12,19 @@ const DEFAULT_WEATHER_IMAGE = 'assets/images/weather.svg';
 })
 export class HeaderComponent implements OnInit {
   public weatherImage = DEFAULT_WEATHER_IMAGE;
+  public whiteHeader = false;
+  private allowedRoutes = TRANSPARENT_HEADER_ROUTES;
 
   constructor(
     private _router: Router,
+    private _route: ActivatedRoute,
     private _location: Location,
     private _weatherQuery: WeatherQueries
   ) {}
 
 
   ngOnInit(): void {
+    this.whiteHeader = !this.allowedRoutes.includes(this._route.snapshot.url.join('/'));
     this._weatherQuery.getWeatherData().subscribe(({ data }) => {
       if (data) {
         this.weatherImage = this._setWeatherImage(data.weather.weather[0].icon);
