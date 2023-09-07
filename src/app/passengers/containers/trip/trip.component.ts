f0b10f1be6be090641fbde9182c42375e33e9712
Jose Ajuria <alejoajuria@gmail.com>
2023-09-07 12:57:58 -0600

    gps implemented, marker added

--- src/app/passengers/containers/trip/trip.component.ts
+++ src/app/passengers/containers/trip/trip.component.ts
@@ -1,8 +1,10 @@
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { GoogleMap } from '@capacitor/google-maps';
+import { Geolocation, Position } from '@capacitor/geolocation';
 import { environment } from 'src/environments/environments';
 
-const IMAGE_URL = 'https://biz-app-bucket.s3.us-east-2.amazonaws.com/iiz-green.png';
+const IMAGE_URL = 'https://biiz-bucket.s3.us-east-2.amazonaws.com/iiz-green.png';
+const MARKER_IMAGE = 'https://biiz-bucket.s3.us-east-2.amazonaws.com/marker.png';
 
 @Component({
   selector: 'app-trip',
@@ -11,21 +13,24 @@ const IMAGE_URL = 'https://biz-app-bucket.s3.us-east-2.amazonaws.com/iiz-green.p
 })
 export class TripComponent implements OnInit {
   public imageUrl = IMAGE_URL;
+  public coords = { latitude: 0, longitude: 0 };
+  private markerImage = MARKER_IMAGE;
   @ViewChild('map', { static: true })
   mapRef!: ElementRef;
   newMap!: GoogleMap;
 
   ngOnInit() {
-    setTimeout(() => {
-      GoogleMap.create({
+    this.setDefaultCoordinates();
+    new Promise((resolve) => setTimeout(resolve, 500))
+    .then(() => {
+      return GoogleMap.create({
         id: 'map',
         element: this.mapRef.nativeElement,
         apiKey: environment.mapsApiKey,
         config: {
           center: {
-            // TODO: remove this hardcoded values when GPS implemented properly
-            lat: 14.060536,
-            lng: -87.241214
+            lat: this.coords.latitude,
+            lng: this.coords.longitude
           },
           zoom: 17,
           clickableIcons: false,
@@ -35,7 +40,25 @@ export class TripComponent implements OnInit {
         },
         language: 'es'
       });
-    }, 500);
+    })
+    .then((map) => {
+      this.newMap = map;
+
+      this.newMap.addMarker({
+        coordinate: {
+          lat: this.coords.latitude,
+          lng: this.coords.longitude
+        },
+        iconUrl: this.markerImage
+      });
+    });
   }
+
+  private async setDefaultCoordinates(): Promise<void> {
+    const coordinates = await Geolocation.getCurrentPosition({
+      enableHighAccuracy: true
+    });
+    this.coords = coordinates.coords;
+  };
 }
 
