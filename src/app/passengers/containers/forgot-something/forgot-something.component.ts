b324e17fb79e363a534147b282680e1abffeadb4
Alejandro Ajuria <alejoajuria@gmail.com>
2023-09-19 18:18:30 -0600

    forgot-something component responsived (#81)

--- src/app/passengers/containers/forgot-something/forgot-something.component.ts
+++ src/app/passengers/containers/forgot-something/forgot-something.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+const MESSAGE = 'Si dejó algo olvidado contactanos lo más pronto posible, y brindenos la mayor cantidad de detalles, Ie conectaremos directamente con su chofer asignado. Puedes también comunicarte vía chat en vivo o llamar a nuestros números de servicio al cliente que 24 horas del día.';
+
 @Component({
   selector: 'app-forgot-something',
   templateUrl: './forgot-something.component.html',
   styleUrls: ['./forgot-something.component.scss']
 })
 export class ForgotSomethingComponent implements OnInit {
+  public message = MESSAGE;
   public forgotSomethingForm: FormGroup = new FormGroup({});
 
   constructor(
