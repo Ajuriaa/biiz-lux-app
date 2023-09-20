2e34c86a3a2a2f730eab06cfe80b883180ddc1d5
Ajuuu <alejoajuria@gmail.com>
2023-09-19 23:43:17 -0600

    accident component responsiveness added

--- src/app/passengers/containers/accident/accident.component.ts
+++ src/app/passengers/containers/accident/accident.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 
+const MESSAGE = 'Si estuvo involucrado en algún accidente con alguno de nuestros choferes, contamos con un seguro de viaje que cubre parte de tus gastos, llena los siguientes datos y luego espera ser contactado por nuestro servicio al cliente.';
 @Component({
   selector: 'app-accident',
   templateUrl: './accident.component.html',
   styleUrls: ['./accident.component.scss']
 })
 export class AccidentComponent {
-  options: string[] = ['SI', 'NO'];
-  selectedValue = 'NO';
-  message = '';
+  public infoMessage = MESSAGE;
+  public selectedValue = 'NO';
+  public message = '';
 
-  changeSelectedValue(newValue: string) {
+  public changeSelectedValue(newValue: string) {
     this.selectedValue = newValue;
   }
 }
