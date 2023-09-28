1816e39f10ed041dccc59c875793360b0b002360
Roger! <icarus.gk@gmail.com>
2023-09-28 01:28:47 -0600

    add: available-drivers component

---
+++ src/app/shared/available-drivers/components/available-drivers.component.ts
@@ -0,0 +1,10 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-available-drivers',
+  templateUrl: './available-drivers.component.html',
+  styleUrls: ['./available-drivers.component.scss']
+})
+export class AvailableDriversComponent {
+
+}
